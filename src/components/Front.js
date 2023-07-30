00d6f60a54f7aa41403f2341c3e4cd488489f149
marketpanda <91251429+marketpanda@users.noreply.github.com>
2023-07-31 04:02:45 +0700

    Delete Front.js

--- src/components/Front.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-import bannner from '../assets/tasks_cartoon.jpg';
-const Front = () => {
-    return (_jsxs(_Fragment, { children: [_jsx("h3", { children: "Welcome to Tasks Manager" }), _jsx("img", { src: bannner, alt: "task manager", width: "500px" })] }));
-};
-export default Front;
